f93cefe1b8e4ac6ec699b031dea995ce81cd25ba
Fib0112358 <wmwallace5@gmail.com>
2016-01-27 10:49:55 -0600

    change

--- arduino_traffic_light/arduino_traffic_light.ino
+++ arduino_traffic_light/arduino_traffic_light.ino
@@ -14,15 +14,43 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_1, HIGH);
-  delay(250);
+  delay(500);
   digitalWrite(LED_1, LOW);
-  delay(250);
+  delay(500);
   digitalWrite(LED_2, HIGH);
-  delay(250);
+  delay(500);
   digitalWrite(LED_2, LOW);
+  delay(500);
+  digitalWrite(LED_3, HIGH);
+  delay(500);
+  digitalWrite(LED_3, LOW);
+  delay(500);
+digitalWrite(LED_1, HIGH);
+  delay(500);
+  digitalWrite(LED_1, LOW);
+  delay(500);
+  digitalWrite(LED_2, HIGH);
+  delay(500);
+  digitalWrite(LED_2, LOW);
+  delay(500);
+  digitalWrite(LED_3, HIGH);
+  delay(500);
+  digitalWrite(LED_3, LOW);
+  delay(500);
+  digitalWrite(LED_3, HIGH);
+  delay(250);
+  digitalWrite(LED_3, LOW);
   delay(250);
   digitalWrite(LED_3, HIGH);
   delay(250);
   digitalWrite(LED_3, LOW);
   delay(250);
-}
+  digitalWrite(LED_3, HIGH);
+  delay(250);
+  digitalWrite(LED_3, LOW);
+  delay(250);
+  digitalWrite(LED_3, HIGH);
+  delay(250);
+  digitalWrite(LED_3, LOW);
+  delay(250);
+ }
